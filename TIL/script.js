804d1c253aaa2c45091b0944476f8026197fcd06
Bruce <irabruce10@gmail.com>
2024-08-03 12:57:47 +0200

    adding js

--- TIL/script.js
+++ TIL/script.js
@@ -1,1 +1,13 @@
-console.log('setting')
+const btn = document.querySelector(".btn-open");
+const form = document.querySelector(".fact-form");
+
+
+
+btn.addEventListener("click", function() {
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+  }else{
+    form.classList.add("hidden");
+  }
+
+});
