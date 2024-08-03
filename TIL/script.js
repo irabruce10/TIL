d3ecad1794a58adc7a62a3fa1dbe94b275d2f6f6
Bruce <irabruce10@gmail.com>
2024-08-03 13:04:21 +0200

    adding js

--- TIL/script.js
+++ TIL/script.js
@@ -1,13 +1,12 @@
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
 
-
-
-btn.addEventListener("click", function() {
+btn.addEventListener("click", function () {
   if (form.classList.contains("hidden")) {
     form.classList.remove("hidden");
-  }else{
+    btn.innerHTML = "close";
+  } else {
     form.classList.add("hidden");
+    btn.innerHTML = "share a fact";
   }
-
 });
