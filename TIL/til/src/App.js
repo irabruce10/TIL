d3d85a78cb1821811429e2599c2400e676cde372
Bruce <irabruce10@gmail.com>
2024-08-05 12:40:43 +0200

    fixing some bugs

--- TIL/til/src/App.js
+++ TIL/til/src/App.js
@@ -14,40 +14,6 @@ const CATEGORIES = [
   { name: "news", color: "#8b5cf6" },
 ];
 
-const initialFacts = [
-  {
-    id: 1,
-    text: "React is being developed by Meta (formerly facebook)",
-    source: "https://opensource.fb.com/",
-    category: "technology",
-    votesInteresting: 24,
-    votesMindblowing: 9,
-    votesFalse: 4,
-    createdIn: 2021,
-  },
-  {
-    id: 2,
-    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-    source:
-      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-    category: "society",
-    votesInteresting: 11,
-    votesMindblowing: 2,
-    votesFalse: 0,
-    createdIn: 2019,
-  },
-  {
-    id: 3,
-    text: "Lisbon is the capital of Portugal",
-    source: "https://en.wikipedia.org/wiki/Lisbon",
-    category: "society",
-    votesInteresting: 8,
-    votesMindblowing: 3,
-    votesFalse: 1,
-    createdIn: 2015,
-  },
-];
-
 function isValidHttpUrl(string) {
   try {
     const newUrl = new URL(string);
@@ -114,14 +80,17 @@ function Header({ openForm, setOpenForm }) {
   return (
     <header className="header">
       <div className="logo">
-        <img src="logo.png" height="68" width="68" alt="Today I Learned Logo" />
-        <h1>T I L</h1>
+        <a href="/">
+          <img src="./images/log.png" alt="Today I Learned Logo" />
+          <h1>TIL</h1>
+        </a>
       </div>
 
       <button
         className="btn btn-large btn-open"
         onClick={() => setOpenForm(!openForm)}>
-        {openForm ? "Close" : "Share a Fact"}
+        {openForm ? "Close" : "Share a Fact"}{" "}
+        <img src="./images/share.png" alt="share a fact" />
       </button>
     </header>
   );
@@ -274,8 +243,12 @@ function Fact({ fact, setFacts }) {
         <button onClick={() => handleVotes("votesInteresting")}>
           👍 {fact.votesInteresting}
         </button>
-        <button onClick={() => handleVotes("votesMindblowing")} >🤯 {fact.votesMindblowing}</button>
-        <button onClick={() => handleVotes("votesFalse")}>⛔️ {fact.votesFalse}</button>
+        <button onClick={() => handleVotes("votesMindblowing")}>
+          🤯 {fact.votesMindblowing}
+        </button>
+        <button onClick={() => handleVotes("votesFalse")}>
+          ⛔️ {fact.votesFalse}
+        </button>
       </div>
     </li>
   );
